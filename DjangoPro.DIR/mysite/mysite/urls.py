53524a4bc6252b3699b54c87e878db7373cf8333
maxi-33 <146625855+maxi-33@users.noreply.github.com>
2024-01-28 14:05:41 +0300

    Add files via upload

---
+++ DjangoPro.DIR/mysite/mysite/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    ProductsList, ProductDetail, ProductCreate, ProductUpdate
+)
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product_list'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+]
